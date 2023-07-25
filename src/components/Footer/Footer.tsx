399923bfb6ce763a608826af59c13652840c9772
Patrycja Tyra <patrycja.tyra@superdry.com>
2023-07-25 21:42:20 +0100

    restyled footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,57 +1,66 @@
+// Footer.tsx
 import React from "react";
-import "./Footer.css"; // Create a CSS file for styling if needed
+import "./Footer.css"; // Import the CSS file for styling
 
 const Footer: React.FC = () => {
+  // Your component logic here
+
   const socialLinks = [
-    { icon: "icon-facebook", href: "https://www.facebook.com/olx.polska" },
-    { icon: "icon-instagram", href: "https://www.instagram.com/olxpolska" },
-    {
-      icon: "icon-youtube",
-      href: "https://www.youtube.com/channel/UCGr18y85p6zXQwBev6CGwow",
-    },
-    { icon: "icon-linkedin", href: "https://pl.linkedin.com/company/olxpraca" },
-    { icon: "icon-pinterest", href: "https://pl.pinterest.com/olxpolska" },
+    { icon: "icon-facebook", href: "https://www.facebook.com/yourpage" },
+    { icon: "icon-twitter", href: "https://www.twitter.com/yourpage" },
+    { icon: "icon-instagram", href: "https://www.instagram.com/yourpage" },
+    // Add more social links as needed
   ];
 
   return (
-    <footer id="footer-container">
-      <div className="footer-top wrapper small clr brbott-12 rel">
-        <div className="content brbott-5">
-          <div className="pding15_0 siteinfobox clr">
-            <div className="social-links clr">
-              <p>Dołącz do nas:</p>
-              <ul>
-                {socialLinks.map((link, index) => (
+      <footer className="footer-container">
+        <div className="footer-top wrapper small clr brbott-12 rel">
+          {/* Your footer content */}
+          <div className="content brbott-5">
+            {/* ... */}
+          </div>
+
+          {/* Contact Us section */}
+          <div className="contact-us">
+            <h3>Contact Us</h3>
+            <p>
+              Email: contact@example.com
+              <br />
+              Phone: +1234567890
+            </p>
+          </div>
+
+          {/* Social links */}
+          <div className="footer-social-links">
+            <h3>Follow Us</h3>
+            <ul>
+              {socialLinks.map((link, index) => (
                   <li key={index}>
                     <a
-                      className={link.icon}
-                      href={link.href}
-                      target="_blank"
-                      rel="noopener noreferrer"
+                        className={link.icon}
+                        href={link.href}
+                        target="_blank"
+                        rel="noopener noreferrer"
                     ></a>
                   </li>
-                ))}
-              </ul>
-            </div>
-
-            <div className="overh text">
-              {/* OLX description and other links */}
-              {/* ... Your OLX description and other links */}
-            </div>
+              ))}
+            </ul>
           </div>
         </div>
-      </div>
 
-      <div className="footer-bottom" id="lastwrapper">
-        <div className="wrapper small">
-          <div className="margintop15 clr rel">
-            {/* Bottom section */}
-            {/* ... Your bottom section content */}
-          </div>
-          <p>&copy; {new Date().getFullYear()} Patrycja.io</p>
+        <div className="footer-logo">
+          {/* Your logo SVG */}
+          {/* ... */}
         </div>
-      </div>
-    </footer>
+
+        <div className="footer-bottom" id="lastwrapper">
+          <div className="wrapper small">
+            {/* Your bottom section content */}
+            {/* ... */}
+            <p>&copy; {new Date().getFullYear()} Patrycja.io</p>
+          </div>
+        </div>
+      </footer>
   );
 };
 
