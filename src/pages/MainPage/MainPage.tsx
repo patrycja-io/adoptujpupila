cbdf03c4146e70aac3b7319354cbfc7bfef244f9
Patrycja Tyra <patrycja.tyra@superdry.com>
2023-07-26 16:22:49 +0100

    added new section to MainPage.tsx

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import "./MainPage.css";
 import Avatar from "../../components/Avatar/Avatar";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 const MainPage: React.FC = () => {
   const personName = "John Doe";
@@ -10,23 +10,23 @@ const MainPage: React.FC = () => {
   const catAvatarsData: { name: string; svgData: string }[] = [
     {
       name: "cat1",
-      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
-    },
-    {
-    name: "cat1",
-          svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+      svgData: '<svg ></svg>',
     },
     {
       name: "cat1",
-      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+      svgData: '<svg ></svg>',
     },
     {
       name: "cat1",
-      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+      svgData: '<svg  ></svg>',
     },
     {
       name: "cat1",
-      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+      svgData: '<svg ></svg>',
+    },
+    {
+      name: "cat1",
+      svgData: '<svg ></svg>',
     },
     // Add more cat avatars data as needed
   ];
@@ -34,7 +34,7 @@ const MainPage: React.FC = () => {
   const dogAvatarsData: { name: string; svgData: string }[] = [
     {
       name: "dog1",
-      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+      svgData: '<svg ></svg>',
     },
     {
       name: "dog1",
@@ -68,6 +68,85 @@ const MainPage: React.FC = () => {
     </div>
   );
 
+  const voivodeShips: { name: string; svgData: string }[] = [
+    {
+      name: "Dolnośląskie",
+      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+    },
+    {
+      name: "Kujawsko-Pomorskie",
+      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+    },
+    {
+      name: "Lubelskie",
+      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+    },
+    {
+      name: "Lubuskie",
+      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+    },
+    {
+      name: "Lódzkie",
+      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+    },
+    {
+      name: "Małopolskie",
+      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+    },
+    {
+      name: "Mazowieckie",
+      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+    },
+    {
+      name: "Opolskie",
+      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+    },
+    {
+      name: "Podkarpackie",
+      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+    },
+    {
+      name: "Podlaskie",
+      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+    },
+    {
+      name: "Pomorskie",
+      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+    },
+    {
+      name: "Sląskie",
+      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+    },
+    {
+      name: "Swiętokrzyskie",
+      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+    },
+    {
+      name: "Warmińsko-mazurskie",
+      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+    },
+    {
+      name: "Wielkopolskie",
+      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+    },
+    {
+      name: "Zachodniopomorskie",
+      svgData: '<svg viewBox="0 0 36 36" fill="none" ... ></svg>',
+    },
+  ];
+  const VoivodeshipsList: React.FC<{
+    shipsData: { name: string; svgData: string }[];
+  }> = ({ shipsData }) => (
+    <div className="avatar-row">
+      {shipsData.map((shipData, index) => (
+        <div key={`${shipData.name}-${index}`} className="avatar-item">
+          {/* Render the SVG component */}
+          <Avatar name={shipData.name} svgData={shipData.svgData} />
+        </div>
+      ))}
+    </div>
+  );
+
   return (
     <Fragment>
       <div className="adoptujteraz-container">
@@ -81,6 +160,13 @@ const MainPage: React.FC = () => {
         <AvatarsList avatarsData={dogAvatarsData} />
       </div>
 
+      <div className="schroniska-container">
+        <header className="banner">
+          <h1 className="centered-text">Schroniska</h1>
+        </header>
+
+        <VoivodeshipsList shipsData={voivodeShips} />
+      </div>
       <div className="promowane-container">
         <header className="banner">
           <h1 className="centered-text">Ogloszenia Promowane</h1>
