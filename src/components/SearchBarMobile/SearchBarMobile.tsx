03b198f35c1144932d6e13c8ee8efe6d24985c39
Patrycja Tyra <patrycja.tyra@superdry.com>
2023-08-07 11:50:59 +0100

    fix searchbar mobile navigate to searchview

--- src/components/SearchBarMobile/SearchBarMobile.tsx
+++ src/components/SearchBarMobile/SearchBarMobile.tsx
@@ -1,8 +1,7 @@
-// SearchBarMobile.tsx
-
 import React, { useState } from "react";
 import styles from "./SearchBarMobile.module.css"; // Import CSS Modules styles
 import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 interface SearchBarMobileProps {
   onSearch: (query: string) => void;
@@ -10,23 +9,24 @@ interface SearchBarMobileProps {
 
 const SearchBarMobile: React.FC<SearchBarMobileProps> = ({ onSearch }) => {
   const [query, setQuery] = useState<string>("");
-
+  const navigate = useNavigate();
   const handleSearch = () => {
     onSearch(query);
+    navigate("/searchView");
   };
 
   return (
-      <div className={styles["search-bar-mobile"]}>
-        <input
-            type="text"
-            placeholder="Szukaj..."
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-        />
-        <div className={styles["search-icon"]} onClick={handleSearch}>
-          <FaSearch />
-        </div>
+    <div className={styles["search-bar-mobile"]}>
+      <input
+        type="text"
+        placeholder="Szukaj..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <div className={styles["search-icon"]} onClick={handleSearch}>
+        <FaSearch />
       </div>
+    </div>
   );
 };
 
